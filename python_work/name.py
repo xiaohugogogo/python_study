b92bfd11f062fd16f4631a180c0f6841073fce7f
xiaohugogogo <yuhuzhao@qq.com>
2017-12-10 16:18:11 +0800

    python string operation

---
+++ python_work/name.py
@@ -0,0 +1,26 @@
+name = "ada lovelace"
+print(name)
+print(name.title())
+
+name = "hELLO worLD"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+
+msg = "Hello, " + full_name.title() + "!"
+print(msg)
+
+
+print("\tPython")
+print("Language:\nPython\nC\nJavaScript")
+
+favorite_language = " python "
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
