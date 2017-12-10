70bef753597aa1a51909760bc32c8a64e4b8d6c7
xiaohugogogo <yuhuzhao@qq.com>
2017-12-10 21:32:45 +0800

    array

---
+++ python_work/bicycles.py
@@ -0,0 +1,10 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[3])
+print(bicycles[-1])
+print(bicycles[-2])
+
+msg = "My first bicycle is a " + bicycles[2].title() + "."
+print(msg)
