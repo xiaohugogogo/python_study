41cb70a16fadb360cd124995257cdec836f4d28f
xiaohugogogo <yuhuzhao@qq.com>
2017-12-10 17:30:13 +0800

    string practise

---
+++ python_work/name_cases.py
@@ -0,0 +1,19 @@
+name = "Eric"
+print("Hello " + name + ", would you like to learn some Python today?")
+
+name = "zHAO yU Hu"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+sentence = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+print(sentence)
+
+famous_person = "Albert Einstein"
+message = "A person who never made a mistake never tried anything new."
+print(famous_person + ' once said, "' + message + '"')
+
+name = " \t David \n "
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
